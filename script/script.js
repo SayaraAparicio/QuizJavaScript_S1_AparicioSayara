a6f2b6e7bac1f84572015540c75f6c160bf814e5
SayaraAparicio <sayara.arciniegas@gmail.com>
2025-04-28 09:38:52 -0500

    feat: :ambulance: help

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const links = document.querySelectorAll('a.boton1, a.boton2, a.boton3');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  links.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === '#' + current) {
+      link.classList.add('active');
+    }
+  });
+});
